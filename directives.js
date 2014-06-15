e96808c627fd307a6a9d46abb94984ee65ef3e02
Androguide <admin@androguide.fr>
2014-06-15 05:10:31 +0200

    dropdowns: add single-button dropdown directive

--- directives.js
+++ directives.js
@@ -21,13 +21,14 @@ angular.module('Angustrap', [])
   // Dropdowns
   // ---------
   // A fully markup-driven directive to abstract
-  // Bootstrap's [dropdown](http://getbootstrap.com/components/#dropdowns) component.<br/><br/>
+  // Bootstrap's [dropdown](http://getbootstrap.com/components/#dropdowns) components.<br/><br/>
   // **Example**:<br/>
   // ```html
-  // <dropdown as-id="'myDropdown'">
+  // <!-- Single-button dropdown -->
+  // <btn-dropdown as-id="'myDropdown'" theme="'warning'">
   //     <dropdown-item as-href="'http://google.com'">Google</dropdown-item>
   //     <dropdown-item as-href="'http://twitter.com'">Twitter</dropdown-item>
-  // </dropdown>
+  // </btn-dropdown>
   // ```
   // _Note that you can still pass the usual ```dropdown-menu-right``` class to right-align the dropdown relatively to its parent_
   .directive('dropdown', function() {
@@ -35,10 +36,9 @@ angular.module('Angustrap', [])
       restrict: 'E',
       replace: true,
       transclude: true,
-      templateUrl: 'templates/dropdowns/dropdowns.html',
+      templateUrl: 'templates/dropdowns/dropdown.html',
       scope: {
-        asId: "=asId",
-        items: "=items"
+        asId: '=asId'
       }
     }
   })
@@ -53,4 +53,21 @@ angular.module('Angustrap', [])
       scope: { asHref: '=asHref' }
     }
   })
+
+  // TODO: Dropdown divider directive
+
+  // Single-button dropdown
+  .directive('btnDropdown', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      templateUrl: 'templates/dropdowns/button-dropdown.html',
+      scope: {
+        asId: '=asId',
+        theme: '=theme'
+      }
+    }
+  })
+
 ;
