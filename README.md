654dfbaa5f69384df21390d8111dde09ae3de324
Androguide <admin@androguide.fr>
2014-06-15 04:39:00 +0200

    dropdowns: use transclusion to improve the directive
    Give more granularity and a more natural syntax to the <dropdown> directive by using transclusion and a new directive: dropdown-item
    
    Example:
    
    <dropdown class="dropdown-menu-right" as-id="'myDropdown'">
        <dropdown-item as-href="'http://google.com'">Google</dropdown-item>
        <dropdown-item as-href="'http://twitter.com'">Twitter</dropdown-item>
    </dropdown>

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 Angustrap
 =========
 
-Concise &amp; Markup-driven AngularJS Directives for Bootstrap
-  
-      
-  
-  
+Concise &amp; Markup-driven AngularJS Directives for Bootstrap.
+
+
 ###Glyphicons
 Shortcut directive for Bootstrap's glyphicons.
 
@@ -16,16 +14,17 @@ Shortcut directive for Bootstrap's glyphicons.
 
 _For the list of all available icons, see http://getbootstrap.com/components/#glyphicons-glyphs_
 
-  
-  
-  
+
 ###Dropdowns
 A fully markup-driven directive to abstract
 Bootstrap's [dropdown](http://getbootstrap.com/components/#dropdowns) component.
 
 **Example**:
 ```html
-<dropdown as-id="'myDropdown'" items="['First', 'Second', 'Third', 'Fourth', 'etc...']"></dropdown>
+<dropdown as-id="'myDropdown'">
+    <dropdown-item as-href="'http://google.com'">Google</dropdown-item>
+    <dropdown-item as-href="'http://twitter.com'">Twitter</dropdown-item>
+</dropdown>
 ```
 
 _Note that you can still pass the usual ```dropdown-menu-right``` class to right-align the dropdown relatively to its parent_
--- directives.js
+++ directives.js
@@ -24,18 +24,33 @@ angular.module('Angustrap', [])
   // Bootstrap's [dropdown](http://getbootstrap.com/components/#dropdowns) component.<br/><br/>
   // **Example**:<br/>
   // ```html
-  // <dropdown as-id="'myDropdown'" items="['First', 'Second', 'Third', 'Fourth', 'etc...']"></dropdown>
+  // <dropdown as-id="'myDropdown'">
+  //     <dropdown-item as-href="'http://google.com'">Google</dropdown-item>
+  //     <dropdown-item as-href="'http://twitter.com'">Twitter</dropdown-item>
+  // </dropdown>
   // ```
   // _Note that you can still pass the usual ```dropdown-menu-right``` class to right-align the dropdown relatively to its parent_
   .directive('dropdown', function() {
     return {
       restrict: 'E',
       replace: true,
-      templateUrl: 'dropdowns.html',
+      transclude: true,
+      templateUrl: 'templates/dropdowns/dropdowns.html',
       scope: {
         asId: "=asId",
         items: "=items"
       }
+    }
+  })
+
+  // This is the `<dropdown-item>` directive used through transclusion by the `<dropdown>` directive
+  .directive('dropdownItem', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      templateUrl: 'templates/dropdowns/dropdown-item.html',
+      scope: { asHref: '=asHref' }
     }
   })
 ;
