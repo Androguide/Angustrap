be8f3b450b396b22f8034a28f0396031b282a165
Androguide <admin@androguide.fr>
2014-06-15 15:11:31 +0200

    docs: get the comments & generated doc up-to-date

--- angustrap.js
+++ angustrap.js
@@ -5,7 +5,7 @@ angular.module('Angustrap', [])
   // Shortcut directive for Bootstrap's glyphicons.<br/><br/>
   // **Example**:<br/>
   // ```html
-  // <glyph icon="'copyright-mark'"></glyph>
+  // <glyph icon="heart"></glyph>
   // ```
   // <br/>
   // _For the list of all available icons, see http://getbootstrap.com/components/#glyphicons-glyphs_
@@ -21,31 +21,12 @@ angular.module('Angustrap', [])
 
   // Dropdowns
   // ---------
-  // A fully markup-driven directive to abstract
+  // Fully markup-driven directives to abstract
   // Bootstrap's [dropdown](http://getbootstrap.com/components/#dropdowns) components.<br/><br/>
-  // **Example**:<br/>
-  // ```html
-  // <!-- Single-button dropdown -->
-  // <btn-dropdown as-id="'myDropdown'" theme="'warning'">
-  //     <dropdown-item as-href="'http://google.com'">Google</dropdown-item>
-  //     <dropdown-item as-href="'http://twitter.com'">Twitter</dropdown-item>
-  // </btn-dropdown>
-  // ```
   // _Note that you can still pass the usual ```dropdown-menu-right``` class to right-align the dropdown relatively to its parent_
-  .directive('dropdown', function() {
-    var defObj = {
-      restrict: 'E',
-      replace: true,
-      transclude: true,
-      templateUrl: 'templates/dropdowns/dropdown.html',
-      scope: {
-        asId: '@asId'
-      }
-    };
-    return defObj;
-  })
 
-  // This is the `<dropdown-item>` directive used through transclusion by the `<dropdown>` directive
+  // ### Dropdown Item
+  // This is the `<dropdown-item>` directive used through transclusion by the other dropdown directives.
   .directive('dropdownItem', function() {
     var defObj = {
       restrict: 'E',
@@ -59,7 +40,36 @@ angular.module('Angustrap', [])
 
   // TODO: Dropdown divider directive
 
+  //
+  // ### Regular Dropdown
+  // **Example**:<br/>
+  // ```html
+  // <dropdown as-id="myDropdown">
+  //     <dropdown-item as-href="http://google.com">Google</dropdown-item>
+  //     <dropdown-item as-href="http://twitter.com">Twitter</dropdown-item>
+  // </dropdown>
+  // ```
+  .directive('dropdown', function() {
+    var defObj = {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      templateUrl: 'templates/dropdowns/dropdown.html',
+      scope: {
+        asId: '@asId'
+      }
+    };
+    return defObj;
+  })
+
   // ### Single-button dropdown
+  // **Example**:<br/>
+  // ```html
+  // <btn-dropdown as-id="myDropdown" theme="danger" size="lg">
+  //     <dropdown-item as-href="http://google.com">Google</dropdown-item>
+  //     <dropdown-item as-href="http://twitter.com">Twitter</dropdown-item>
+  // </btn-dropdown>
+  // ```
   .directive('btnDropdown', function($timeout) {
     var defObj = {
       restrict: 'E',
@@ -72,20 +82,20 @@ angular.module('Angustrap', [])
         size: '@size'
       },
       link: function(scope, el, attrs) {
-        // Specifying an ID is mandatory
-        if (typeof attrs.asId === 'undefined') {
+        // Specifying an `as-id="someId"` attribute is mandatory, return an error if none was found or if it's empty.
+        if (typeof attrs.asId === 'undefined' || attrs.asId === '') {
           return console.log('Angustrap: ERROR - You must define an ID for this dropdown, using the as-id="yourId" attribute');
         }
-        // Set the theme to .btn-default if no theme attribute is specified
-        if (typeof attrs.theme === 'undefined') attrs.theme = 'default';
+
+        // If no `theme="primary|success|info|warning|danger"` attribute was passed, we default back to the `.btn-default` class.
+        if (typeof attrs.theme === 'undefined' || attrs.theme === '') attrs.theme = 'default';
 
         // Clean-up empty class prefix if no size attribute is specified
         var handleSize = function() {
-          if (typeof attrs.size === 'undefined') {
+          if (typeof attrs.size === 'undefined' || attrs.size === '')
             angular.element('#' + attrs.asId).removeClass('btn-');
-          }
         }
-        // This is a workaround so that our function is not executed before the DOM is loaded
+        // This $timeout is a workaround so that our function is not executed before the DOM is loaded
         $timeout(handleSize, 0);
       }
     };
