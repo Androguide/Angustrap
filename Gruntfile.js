9cf12391e564f7db6506e9e53f5dedcb17612cf7
Louis Teboul <louis.teboul@izi-pass.com>
2014-06-19 12:46:02 +0200

    Grunt: add tasks to watch and compile CoffeeScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,34 @@ module.exports = function(grunt) {
                 src: '<%= pkg.name %>.js',
                 dest: 'app/<%= pkg.name %>.min.js'
             }
+        },
+    
+
+        coffee: {
+           compileWithMaps: {
+                options: {
+                  join: true
+                },
+                files: {
+                  '<%= pkg.name %>.js': '<%= pkg.name %>.coffee' // 1:1 compile
+                  //'<%= pkg.name %>.combined.js': ['directives/*/*.coffee', 'directives/*/*.coffee'] // concat then compile into single file
+                }
+            }
+        },
+
+        watch: {
+          scripts: {
+            files: ['angustrap.js'],
+            tasks: ['uglify']
+          }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-
+    grunt.registerTask('default', ['coffee', 'uglify']);
 };
