0ffa5ea8f1529257c4c65b1a69f172bbd9b1e55c
Louis Teboul (Androguide) <admin@androguide.fr>
2014-06-24 19:48:56 +0200

    remove left-over angustrap.coffee.bak file

--- angustrap.combined.js
+++ angustrap.combined.js
@@ -226,7 +226,7 @@
           maxValue: "@",
           asTitle: "@"
         },
-        template: '<div class="progress"> <div class="progress-bar progress-bar-{{theme}}" role="progressbar" aria-valuenow="{{currValue}}" aria-valuemin="{{minValue}}" aria-valuemax="{{maxValue}}" style="width: {{currValue}}%"> <span class="sr-only">{{asTitle}}</span> </div> </div>'
+        template: '<div class="progress"> <div class="progress-bar progress-bar-{{theme}}" role="progressbar" aria-valuenow="{{currValue}}" aria-valuemin="{{minValue}}" aria-valuemax="{{maxValue}}" style="width: {{currValue}}%"> <span class="sr-only">{{asTitle}}</span> </div> </div'
       };
     }
   ]);
